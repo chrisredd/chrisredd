dc65a9b53c34df7b75d6ac6e338a34ccf98d0136
Chris Redd <credd@sandiego.edu>
2023-07-28 02:57:49 -0700

    <br>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,6 @@ const Home = () => {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <br>
-      </br>
-      <br>
-      </br>
       
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
